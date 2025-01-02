44ad6ea4b608f77eaf3d5e043a6ae9345ad23f42
erenalts <erenaltas48@gmail.com>
2025-01-02 05:16:19 +0300

    Add files via upload

---
+++ 8.if else yapısı/app.js
@@ -0,0 +1,31 @@
+/* if-else bir karar yapısıdır
+   İlk satır if ile başlar son satır else ile biter arada da birden çok koşul satırı varsa else if ile başlar ardından koş gelir
+   if tek başına bulunabilir
+   else tek başına bulunamaz
+
+   aynanda birden fazla koşulun çalıştırılması istenilirse 
+   if(not > 50){
+    console.log("GEÇTİNİZ");
+    }
+
+    if()  {}
+
+
+    if()  {}
+
+
+    if()  {}
+
+
+
+ardarda birden çok if bloğu açılabilir doğruysa blok çalışır bir sonrakine geçer o da doğruysa onu da çalıştırır 
+  */
+
+let not = 55;
+
+if(not > 50){
+console.log("GEÇTİNİZ");
+} else{
+    console.log("KALDINIZ")
+}
+
